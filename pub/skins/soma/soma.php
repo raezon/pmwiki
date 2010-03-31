969a02739ec802917af101702e541c8891f133f9
Julian_Chu <walkingice@0xlab.org>
2010-03-31 15:33:23 +0800

    Add Skin: Soma
    http://www.pmwiki.org/wiki/Cookbook/Soma
    
    edit local/config.php to enable
    
    $Skin = 'soma';

---
+++ pub/skins/soma/soma.php
@@ -0,0 +1,13 @@
+<?PHP
+
+global $WikiSubTitle, $WikiTitle, $WikiRightsMsg;
+
+
+SDV ($WikiSubTitle, "colorful skin");
+SDV ($WikiTitle, "S o m a ");
+SDV ($WikiRightsMsg, "<p> Powered by PmWiki </p>");
+
+## Remove the line below
+
+$WikiTitle = "Soma" ;
+?>
