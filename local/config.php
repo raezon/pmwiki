d91b7cf1cb40d9f2d815ff09de6657f282a81f14
Julian_Chu <walkingice@0xlab.org>
2010-03-31 14:53:05 +0800

    Initialize Version 2.2.15 (2010-03-27)
    Add Chinese Traditional language

---
+++ local/config.php
@@ -0,0 +1,18 @@
+<?php if (!defined('PmWiki')) exit();
+$WikiTitle = "Brainless";
+#$PageLogoUrl = "http://example.com/mylogo.gif";
+
+# Uncomment these if needed
+#$ScriptUrl = 'http://example.com/pmwiki/pmwiki.php';
+#$PubDirUrl = 'http://example.com/pmwiki/pub';
+
+$DefaultPasswords['admin'] = crypt('12345');
+
+$EnableUpload = 1;
+$DefaultPasswords['upload'] = crypt('upload');
+
+XLPage('fr','PmWikiZhTw.XLPage');
+
+putenv("TZ=EST5EDT");
+$TimeFmt = '%B %d, %Y, at %I:%M %p EST';
+?>
