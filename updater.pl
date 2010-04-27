f6c2ec1270b82ccfe0147960e10c7ebb2488387f
Julian_Chu <walkingice@0xlab.org>
2010-04-28 01:36:57 +0800

    Update Script for path
    If you add this script into crontab, the script needs
    to use absolutly path.

--- updater.pl
+++ updater.pl
@@ -8,15 +8,20 @@ my $REMOTE_BRANCH = 'surgery';
 
 ################################
 
+use File::Basename;
+my $filename = $0;
+my $where = dirname($filename);
+chdir $where;
+
 &create_branch if not &branch_exist;
 &commit_changes;
 &push_to_remote;
 
 sub create_branch() {
     print "Create branch: git checkout -b $BRANCH\n";
-    `git checkout -b $BRANCH`;
+    print `git checkout -b $BRANCH`;
     print "Create remote: git remote add $NAME $REMOTE\n";
-    `git remote add $NAME $REMOTE`;
+    print `git remote add $NAME $REMOTE`;
 }
 
 sub branch_exist() {
@@ -31,18 +36,14 @@ sub branch_exist() {
 
 sub commit_changes {
     chomp (my $date = `date -R`);
-    print "git checkout $BRANCH";
-    print "git add uploads/*\n";
-    print "git add wiki.d/*\n";
-    print "git commit -m \"$date\"\n";
 
-    `git checkout $BRANCH`;
-    `git add uploads/*`;
-    `git add wiki.d/*`;
-    `git commit -m "$date"`;
+    print `git checkout $BRANCH`;
+    print `git add uploads/*`;
+    print `git add wiki.d/*`;
+    print `git add *`;
+    print `git commit -m "$date"`;
 }
 
 sub push_to_remote {
-    print "git push $NAME $BRANCH:$REMOTE_BRANCH\n";
-    `git push $NAME $BRANCH:$REMOTE_BRANCH`;
+    print `git push $NAME $BRANCH:$REMOTE_BRANCH`;
 }
